96956721f0b58980b943f5dc13428f492e80ec58
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-02-04 11:57:22 -0600

    feat: feat: integrate questions store and add fetch functionality to StartButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
 import MainHeader from "./components/header/mainHeader/MainHeader";
+import { useQuestionsStore } from "./store/Questions";
 
 function App() {
+
+  const questions = useQuestionsStore(state => state.questions)
+  console.log(questions)
+
   return (
     <main>
       <MainHeader />
--- src/components/common/button/startButton/StartButton.tsx
+++ src/components/common/button/startButton/StartButton.tsx
@@ -1,5 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { useQuestionsStore } from "@/store/Questions";
 
 export const StartButton = () => {
-  return <Button className="border border-slate-800 rounded-[4px]">comenzar</Button>;
+  const LMIT_QUESTIONS = 10;
+  const fetchQuestions = useQuestionsStore((state) => state.fetchQuestions);
+
+  const handleClick = () => {
+    //slint-disable-next-line
+    fetchQuestions(LMIT_QUESTIONS);
+  }
+
+  return <Button onClick={handleClick} className="border border-slate-800 rounded-[4px]" >comenzar</Button>;
 };
