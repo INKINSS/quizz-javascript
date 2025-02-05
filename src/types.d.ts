c0e623c7bc9ef0a4f9e7b699f3189079bbe4eade
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-02-05 17:26:54 -0600

    refactor(data and typed): rename fields in Question interface for clarity
    refactor structure of data.json and types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,10 @@
 export interface Question {
     id: number;
-    pregunta: string;
-    codigo: string;
-    respuestas: string[];
-    respuesta_correcta: string;
-    dificultad: string;
+    question: string;
+    code: string;
+    answers: string[];
+    correctAnswer: number;
+    dificulty: string;
     userSelectedAnswer?: number;
     isCorrectUserAnswer?: boolean;
 }
