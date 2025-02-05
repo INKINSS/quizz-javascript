e373d9d8d781cc909b994347cf7a8a1cca530253
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-02-05 17:28:13 -0600

    feat: enhance question component with answer selection and navigation functionality
    styles for a selected option based on the correct answer to the question

--- src/components/game/ListQuestions.tsx
+++ src/components/game/ListQuestions.tsx
@@ -2,25 +2,55 @@ import { type Question as QuestionType } from "../../types";
 import { Card } from "../ui/card";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { lioshi } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { useQuestionsStore } from "../../store/Questions";
+
+const getBackgroundColor = (info: QuestionType, index: number) => {
+  const { userSelectedAnswer, isCorrectUserAnswer } = info;
+  // el usuario no ha seleccionado nada
+  if (userSelectedAnswer === undefined) return "transparent";
+  // si ya seleccionó y la respuesta es correcta
+  if (userSelectedAnswer === index && isCorrectUserAnswer) return "bg-green-200";
+  // si ya seleccionó pero la respuesta es incorrecta
+  if (userSelectedAnswer === index && !isCorrectUserAnswer) return "bg-red-200";
+  //si el usuario seleccionó la respuesta incorrecta y la respuesta correcta es otra
+  if (index === info.correctAnswer) return "bg-green-200";
+  return "bg-transparent";
+};
 
 export const Question = ({ info }: { info: QuestionType }) => {
+  const selectAnswer = useQuestionsStore((state) => state.selectAnswer);
+  const createHandleClick = (index: number) => () => {
+    if (selectAnswer) {
+      selectAnswer(info.id, index);
+    }
+  };
+
   return (
     <Card className="border-none">
       <span className="w-full text-slate-800 text-[1.2rem] font-semibold text-center inline-block">
-        {info.pregunta}
+        {info.question}
       </span>
-      <SyntaxHighlighter customStyle={{ marginTop:'10px' }} style={lioshi} language="javascript">
-        {info.codigo}
+      <SyntaxHighlighter
+        customStyle={{ marginTop: "10px" }}
+        style={lioshi}
+        language="javascript"
+      >
+        {info.code}
       </SyntaxHighlighter>
-      <div>
-        { info.respuestas.map((respuesta, index) => (
+      <div className="mt-4">
+        {info.answers.map((respuesta, index) => (
           <button
+          disabled={info.userSelectedAnswer !== undefined}
             key={index}
-            className="w-full py-2 text-slate-800 text-[1.2rem] font-normal text-center inline-block hover:bg-slate-200"
+            onClick={createHandleClick(index)}
+            className={`w-full p-2 text-left hover:bg-slate-300 ${getBackgroundColor(
+              info,
+              index
+            )}`}
           >
             {respuesta}
           </button>
-        )) }
+        ))}
       </div>
     </Card>
   );
--- src/store/Questions.tsx
+++ src/store/Questions.tsx
@@ -3,11 +3,14 @@ import { type Question } from "../types";
 
 interface State {
   questions: Question[];
-  currentQuestionIndex: 0;
+  currentQuestionIndex: number;
   fetchQuestions: (limit: number) => Promise<void>;
+  selectAnswer?: (questionId: number, answerIndex: number) => void;
+  goNextQuestion?: () => void;
+  goPrevQuestion?: () => void;
 }
 
-export const useQuestionsStore = create<State>((set) => ({
+export const useQuestionsStore = create<State>((set, get) => ({
   questions: [],
   currentQuestionIndex: 0,
   fetchQuestions: async (limit) => {
@@ -15,5 +18,35 @@ export const useQuestionsStore = create<State>((set) => ({
     const data = await response.json();
     const questions = data.sort(() => Math.random() - 0.5).slice(0, limit);
     set({ questions });
+  },
+  //usar el structureClone para no mutar el estado
+  selectAnswer: (questionId: number, answerIndex: number) => {
+    const { questions } = get();
+    const newQuestions = structuredClone(questions);
+    const questionIndex = newQuestions.findIndex((q) => q.id === questionId);
+    const questionInfo = newQuestions[questionIndex];
+    const isCorrectUserAnswer =
+      questionInfo.correctAnswer === answerIndex;
+    //cambiar la informacion en la copia de la pregunta
+    newQuestions[questionIndex] = {
+      ...questionInfo,
+      isCorrectUserAnswer,
+      userSelectedAnswer: answerIndex,
+    };
+    set({ questions: newQuestions });
+  },
+  goNextQuestion: () => {
+    const { questions, currentQuestionIndex } = get();
+    const nextQuestionIndex = currentQuestionIndex + 1;
+    if(nextQuestionIndex < questions.length){
+      set({ currentQuestionIndex: nextQuestionIndex });
+    }
+  },
+  goPrevQuestion: () => {
+    const { currentQuestionIndex } = get();
+    const prevQuestionIndex = currentQuestionIndex - 1;
+    if(prevQuestionIndex >= 0){
+      set({ currentQuestionIndex: prevQuestionIndex });
+    }
   },
 }));
