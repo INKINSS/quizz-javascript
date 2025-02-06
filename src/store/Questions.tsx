61fff504e983b60a48ebbe4a9ebe7063eabfa276
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-02-06 15:53:29 -0600

    feat: update title in index.html and modify fetch URL in Questions store

--- src/store/Questions.tsx
+++ src/store/Questions.tsx
@@ -13,43 +13,50 @@ interface State {
   difficulty?: string;
 }
 
-export const useQuestionsStore = create<State>()(persist((set, get) => ({
-  questions: [],
-  currentQuestionIndex: 0,
-  fetchQuestions: async (limit) => {
-    const response = await fetch("http://localhost:5173/data.json", { mode: "no-cors" });
-    const data = await response.json();
-    const questions = data.sort(() => Math.random() - 0.5).slice(0, limit);
-    set({ questions });
-  },
-  selectAnswer: (questionId: number, answerIndex: number) => {
-    const { questions } = get();
-    const newQuestions = structuredClone(questions);
-    const questionIndex = newQuestions.findIndex((q) => q.id === questionId);
-    const questionInfo = newQuestions[questionIndex];
-    const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex;
-    newQuestions[questionIndex] = {
-      ...questionInfo,
-      isCorrectUserAnswer,
-      userSelectedAnswer: answerIndex,
-    };
-    set({ questions: newQuestions });
-  },
-  goNextQuestion: () => {
-    const { questions, currentQuestionIndex } = get();
-    const nextQuestionIndex = currentQuestionIndex + 1;
-    if (nextQuestionIndex < questions.length) {
-      set({ currentQuestionIndex: nextQuestionIndex });
-    }
-  },
-  goPrevQuestion: () => {
-    const { currentQuestionIndex } = get();
-    const prevQuestionIndex = currentQuestionIndex - 1;
-    if (prevQuestionIndex >= 0) {
-      set({ currentQuestionIndex: prevQuestionIndex });
-    }
-  },  
-restart: () => {
-    set({ currentQuestionIndex: 0, questions: [] });
-  }
-}), { name: "questions" }));
+export const useQuestionsStore = create<State>()(
+  persist(
+    (set, get) => ({
+      questions: [],
+      currentQuestionIndex: 0,
+      fetchQuestions: async (limit) => {
+        const response = await fetch("/public/data.json", { mode: "no-cors" });
+        const data = await response.json();
+        const questions = data.sort(() => Math.random() - 0.5).slice(0, limit);
+        set({ questions });
+      },
+      selectAnswer: (questionId: number, answerIndex: number) => {
+        const { questions } = get();
+        const newQuestions = structuredClone(questions);
+        const questionIndex = newQuestions.findIndex(
+          (q) => q.id === questionId
+        );
+        const questionInfo = newQuestions[questionIndex];
+        const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex;
+        newQuestions[questionIndex] = {
+          ...questionInfo,
+          isCorrectUserAnswer,
+          userSelectedAnswer: answerIndex,
+        };
+        set({ questions: newQuestions });
+      },
+      goNextQuestion: () => {
+        const { questions, currentQuestionIndex } = get();
+        const nextQuestionIndex = currentQuestionIndex + 1;
+        if (nextQuestionIndex < questions.length) {
+          set({ currentQuestionIndex: nextQuestionIndex });
+        }
+      },
+      goPrevQuestion: () => {
+        const { currentQuestionIndex } = get();
+        const prevQuestionIndex = currentQuestionIndex - 1;
+        if (prevQuestionIndex >= 0) {
+          set({ currentQuestionIndex: prevQuestionIndex });
+        }
+      },
+      restart: () => {
+        set({ currentQuestionIndex: 0, questions: [] });
+      },
+    }),
+    { name: "questions" }
+  )
+);
